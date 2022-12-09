0dd2994b2b10afcc2731711256e03f4cf26d5ac3
Pietro-SA <pietrovincenzo@yahoo.com>
2022-12-09 10:21:43 +0100

    Proejct created

---
+++ src/test/java/com/myjavap/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjavap.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
